61711feb9866b907ac9e9401805cd99aa152dc08
Michael Nyika <michael.nyika@king.com>
2017-01-20 13:28:00 +0100

    we don't need tabs at the beginning of lines

--- Haskell/Sudoku/Sudoku.hs
+++ Haskell/Sudoku/Sudoku.hs
@@ -13,30 +13,30 @@ data SudoCell = SudoCell (XLoc , YLoc , SValue, Region, Found) deriving (Eq, Sho
 --param: the starting position of the lower left corner (1 will create cells, that have 1,1 as lower left corner of the board)
 createBoard :: Int -> [SudoCell]
 createBoard p
-	| p > 9     = []
-	| otherwise = 
-		let noRegionBoard = (map (\x -> SudoCell (x, p, 0, 0, False)) [1..9]) ++ createBoard (p+1) in 
-		fillInRegions noRegionBoard
+ | p > 9     = []
+ | otherwise = 
+ let noRegionBoard = (map (\x -> SudoCell (x, p, 0, 0, False)) [1..9]) ++ createBoard (p+1) in 
+ fillInRegions noRegionBoard
 
 --PRIVATE FUNCTION: USED IN THE createBoard Function to fill in the regions data for each cell
 --function to fill in the region or block for each cell
 fillInRegions :: [SudoCell] -> [SudoCell]
 fillInRegions celldata =
-	let regiondata = concat $ (concat $ replicate 3 $ map (\x -> replicate 3 x) [1,2,3]) ++ (concat $ replicate 3 $ map (\x -> replicate 3 x) [4,5,6])  ++ (concat $ replicate 3 $ map (\x -> replicate 3 x) [7,8,9])
-	in zipWith (\(SudoCell (a, b, c, _, d)) r -> SudoCell (a, b, c, r, d)) celldata regiondata
+ let regiondata = concat $ (concat $ replicate 3 $ map (\x -> replicate 3 x) [1,2,3]) ++ (concat $ replicate 3 $ map (\x -> replicate 3 x) [4,5,6])  ++ (concat $ replicate 3 $ map (\x -> replicate 3 x) [7,8,9])
+ in zipWith (\(SudoCell (a, b, c, _, d)) r -> SudoCell (a, b, c, r, d)) celldata regiondata
 
 --function to give the list of SudoCells that are in the same row as the given SudoCell, all except the row that is used as the reference request
 sameRowCells :: SudoCell -> [SudoCell] -> [SudoCell]
 sameRowCells (SudoCell (a, b, c, d, f)) board = 
-	let cells = filter (\(SudoCell (_, e, _, _, _)) -> (b == e)) board in
-	filter (\g -> (g /= (SudoCell (a, b, c, d, f))) ) cells
+ let cells = filter (\(SudoCell (_, e, _, _, _)) -> (b == e)) board in
+ filter (\g -> (g /= (SudoCell (a, b, c, d, f))) ) cells
 
 
 --function to give the list of SudoCells that are in the same column as the given SudoCell, all except the row that is used as the reference request
 sameColumnCells :: SudoCell -> [SudoCell] -> [SudoCell]
 sameColumnCells (SudoCell (a, b, c, d, f)) board = 
-	let cells = filter (\(SudoCell (e, _, _, _, _)) -> (a == e)) board in
-	filter (\g -> (g /= (SudoCell (a, b, c, d, f))) ) cells
+ let cells = filter (\(SudoCell (e, _, _, _, _)) -> (a == e)) board in
+ filter (\g -> (g /= (SudoCell (a, b, c, d, f))) ) cells
 
 
 --function to check if SudoCell to the LEFT of the current cell has the same svalue
