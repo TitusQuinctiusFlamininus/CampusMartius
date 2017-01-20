cb2e9c030550d8b1b8a24f930056a903cd415940
Michael Nyika <michael.nyika@king.com>
2017-01-20 13:46:24 +0100

    little cleaner code, with some documentation

--- Haskell/Sudoku/Sudoku.hs
+++ Haskell/Sudoku/Sudoku.hs
@@ -18,12 +18,17 @@ createBoard p
  let noRegionBoard = (map (\x -> SudoCell (x, p, 0, 0, False)) [1..9]) ++ createBoard (p+1) in 
  fillInRegions noRegionBoard
 
---PRIVATE FUNCTION: USED IN THE createBoard Function to fill in the regions data for each cell
+--PRIVATE FUNCTION: Used in the createBoard Function to fill in the regions data for each cell
 --function to fill in the region or block for each cell
 fillInRegions :: [SudoCell] -> [SudoCell]
 fillInRegions celldata =
- let regiondata = concat $ (concat $ replicate 3 $ map (\x -> replicate 3 x) [1,2,3]) ++ (concat $ replicate 3 $ map (\x -> replicate 3 x) [4,5,6])  ++ (concat $ replicate 3 $ map (\x -> replicate 3 x) [7,8,9])
+ let regiondata = (createRawRegionValues [1,2,3]) ++ (createRawRegionValues [4,5,6]) ++ (createRawRegionValues [7,8,9])
  in zipWith (\(SudoCell (a, b, c, _, d)) r -> SudoCell (a, b, c, r, d)) celldata regiondata
+
+--PRIVATE FUNCTION: Used in the fillInRegions function to give back a list like this, given [1,2,3]: [1,1,1,2,2,2,3,3,3,1,1,1,2,2,2,3,3,3,1,1,1,2,2,2,3,3,3]
+-- the value 3 used in the function is the height of any 1 region on the sudoku board, a region comprising of a 3x3 square of sudoku cells
+createRawRegionValues :: [Int] -> [Int]
+createRawRegionValues r = concat $ concat $ replicate 3 $ map (\x -> replicate 3 x) r
 
 --function to give the list of SudoCells that are in the same row as the given SudoCell, all except the row that is used as the reference request
 sameRowCells :: SudoCell -> [SudoCell] -> [SudoCell]
@@ -39,7 +44,7 @@ sameColumnCells (SudoCell (a, b, c, d, f)) board =
  filter (\g -> (g /= (SudoCell (a, b, c, d, f))) ) cells
 
 
---function to check if SudoCell to the LEFT of the current cell has the same svalue
+--function to check if SudoCell to the LEFT of the current cell has the same Svalue
 --Param1: The Reference Cell. We want to check all cells to the left of this one
 --Param2: the list of SudoCells that have the same y value
 --checkLeftForValue :: SudoCell -> [SudoCell] -> Bool
