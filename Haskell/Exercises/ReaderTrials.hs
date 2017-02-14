22be8563f6bc8617807afa5d8291100d6c5eb728
Titus Flamininus <mnyika@gmail.com>
2017-02-14 21:28:51 +0100

    Example of how to use the Reader monad

---
+++ Haskell/Exercises/ReaderTrials.hs
@@ -0,0 +1,18 @@
+import Control.Monad.Reader
+import Data.Map as M
+import Data.Maybe as May
+
+type Bindings = Map String Int;
+
+myDb = [("val1", 34), ("val2", 89),("val3", 4),("val4", 1007),("val5", 678)]
+
+process :: String -> Reader Bindings Int
+process key = do
+           env <- ask
+           let val1 = May.fromJust (M.lookup key env) in
+               return (val1)
+
+main :: IO()
+main = do
+         let finale = runReader (process "val4") (M.fromList myDb) in
+             putStrLn (show finale)
