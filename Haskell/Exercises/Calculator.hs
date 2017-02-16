66f694439c5f240528655470d2eca3d39a1fedc4
Titus Flamininus <mnyika@gmail.com>
2017-02-16 22:27:05 +0100

    ok!

--- Haskell/Exercises/Calculator.hs
+++ Haskell/Exercises/Calculator.hs
@@ -3,7 +3,7 @@
 
 import Data.Char
 import Control.Monad.Trans(lift)
-import Control.Monad.Trans.State.Lazy (StateT, put, execStateT)
+import Control.Monad.Trans.State.Lazy (StateT, put, execStateT, get)
 
 type Vsf = Int --the value so far 
 type Computations = Int --number of times we have added
@@ -13,17 +13,17 @@ type CalcState = (Vsf, Computations)
 type Calculation a = StateT CalcState IO a
 
 
-performCalc :: CalcState -> Calculation ()
-performCalc calc@(i,n) = do
+performCalc :: Calculation ()
+performCalc = do
+                           (i,n) <- get
                            lift $ putStrLn "Put in a number or 'end' if you are done"
                            userInput <- lift $ getLine
                            if userInput /= "end" then
-                              let newstate = (i+(read userInput :: Int), n+1) in
-                              performCalc newstate
-                           else put calc
+                              put (i+(read userInput :: Int), n+1)
+                           else put (i,n)
 
 
 main :: IO()
 main = do
-         do val <- execStateT (performCalc (0,0)) (0,0)
+         do val <- execStateT performCalc (0,0)
             putStrLn (show val)
