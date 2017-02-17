32a13ddba4f4a33af788013d4939c5e973a94812
Titus Flamininus <mnyika@gmail.com>
2017-02-17 07:15:34 +0100

    Cool, we can do it with iteration

--- Haskell/Exercises/StateTMonadTransformer_StackedOnIOMonad.hs
+++ Haskell/Exercises/StateTMonadTransformer_StackedOnIOMonad.hs
@@ -8,22 +8,27 @@ import Control.Monad.Trans.State.Lazy (StateT, put, execStateT, get)
 type Vsf = Int --the value so far 
 type Computations = Int --number of times we have added
 
-type CalcState = (Vsf, Computations)
+type CalcState = (Vsf, Computations, Int)
 
 type Calculation a = StateT CalcState IO a
 
 
 performCalc :: Calculation ()
 performCalc = do
-                           (i,n) <- get
-                           lift $ putStrLn "Put in a number or 'end' if you are done"
+                           (i,n,z) <- get
                            userInput <- lift $ getLine
                            if userInput /= "end" then
-                              put (i+(read userInput :: Int), n+1)
-                           else put (i,n)
+                              put (i+(read userInput :: Int), n+1,1)
+                           else put (i,n,0)
+
+doit :: CalcState -> IO ()
+doit state = do 
+                   putStrLn "Put in a number or 'end' if you are done"
+                   (a,b,c) <- execStateT performCalc state
+                   if c == 0 then
+                       putStrLn (show (a,b,c))
+                   else doit (a,b,c)
 
 
 main :: IO()
-main = do
-         do val <- execStateT performCalc (0,0)
-            putStrLn (show val)
+main = do doit (0,0,1)
