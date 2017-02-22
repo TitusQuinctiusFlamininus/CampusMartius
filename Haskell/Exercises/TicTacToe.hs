a0ca9e53c86ba12f85528f9e605dea6c6b3e7a16
Michael Nyika <michael.nyika@king.com>
2017-02-22 18:19:01 +0100

    oh brackets

--- Haskell/Exercises/TicTacToe.hs
+++ Haskell/Exercises/TicTacToe.hs
@@ -10,11 +10,8 @@ instance Eq T3Input where
     X == X = True
     O == O = True
     N == N = True
- 
 
 type T3Cell =  (Int, Int, T3Input)
-
-board = [(1,1,N),(2,1,N),(3,1,N),(1,2,N),(2,2,N),(3,2,N),(1,3,N),(2,3,N),(1,3,N)]
 
 type TicTacToeState a = StateT [T3Cell] IO a
 
@@ -24,15 +21,17 @@ replaceCellInBoard (a,b,i) board =
 
 checkVictory :: [T3Cell] -> String
 checkVictory board@((a,b,c):ys)
- | ((a==1) && (b==1) && (c==X)) && ((a==2) && (b==1) && (c==X)) && ((a==3) && (b==1) && (c==X)) = "You Won!"
- | (a==1 && b==2 && c==X) && (a==2 && b==2 && c==X) && (a==3 && b==2 && c==X) = "You Won!"
- | (a==1 && b==3 && c==X) && (a==2 && b==3 && c==X) && (a==3 && b==3 && c==X) = "You Won!"
- | (a==1 && b==1 && c==X) && (a==1 && b==2 && c==X) && (a==1 && b==3 && c==X) = "You Won!"
- | (a==2 && b==1 && c==X) && (a==2 && b==2 && c==X) && (a==2 && b==3 && c==X) = "You Won!"
- | (a==3 && b==1 && c==X) && (a==3 && b==2 && c==X) && (a==3 && b==3 && c==X) = "You Won!"
- | (a==1 && b==1 && c==X) && (a==2 && b==2 && c==X) && (a==3 && b==3 && c==X) = "You Won!"
- | (a==1 && b==3 && c==X) && (a==2 && b==2 && c==X) && (a==1 && b==1 && c==X) = "You Won!"
+ | ((a==1 && b==1 && c==X) && (a==2 && b==1 && c==X) && (a==3 && b==1 && c==X)) = "You Won!"
+ | ((a==1 && b==2 && c==X) && (a==2 && b==2 && c==X) && (a==3 && b==2 && c==X)) = "You Won!"
+ | ((a==1 && b==3 && c==X) && (a==2 && b==3 && c==X) && (a==3 && b==3 && c==X)) = "You Won!"
+ | ((a==1 && b==1 && c==X) && (a==1 && b==2 && c==X) && (a==1 && b==3 && c==X)) = "You Won!"
+ | ((a==2 && b==1 && c==X) && (a==2 && b==2 && c==X) && (a==2 && b==3 && c==X)) = "You Won!"
+ | ((a==3 && b==1 && c==X) && (a==3 && b==2 && c==X) && (a==3 && b==3 && c==X)) = "You Won!"
+ | ((a==1 && b==1 && c==X) && (a==2 && b==2 && c==X) && (a==3 && b==3 && c==X)) = "You Won!"
+ | ((a==1 && b==3 && c==X) && (a==2 && b==2 && c==X) && (a==1 && b==1 && c==X)) = "You Won!"
  | otherwise                                                                  = if (all (\(_,_,e) -> e == X || e == O) board) then "Noboby Wins, Too bad." else "Keep Playing!...."
+
+board = [(1,1,N),(2,1,N),(3,1,N),(1,2,N),(2,2,N),(3,2,N),(1,3,N),(2,3,N),(1,3,N)]
 
 --runTicTacToe :: TicTacToeState ()
 --runTicTacToe = do
