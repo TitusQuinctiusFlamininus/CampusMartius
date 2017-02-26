c266373e71726b9c1d02f683ba603dc9a3cadbe4
Titus Flamininus <mnyika@gmail.com>
2017-02-26 12:45:35 +0100

    clearer once we use atom

--- Haskell/Exercises/ConfigurationRead_ReaderMonad.hs
+++ Haskell/Exercises/ConfigurationRead_ReaderMonad.hs
@@ -1,16 +1,20 @@
 import Control.Monad.Trans.Reader
 import Data.Map as M
-import Data.Maybe as May
+
 
 type Bindings = Map String Int;
 
+type Mydatabase a = Reader Bindings a
+
 myDb = [("val1", 34), ("val2", 89),("val3", 4),("val4", 1007),("val5", 678)]
 
-process :: String -> Reader Bindings Int
+process :: String -> Mydatabase Int
 process key = do
            env <- ask
-           let val1 = May.fromJust (M.lookup key env) in
-               return (val1)
+           let lookedup = (M.lookup key env) in
+            case lookedup of
+              Nothing -> return 0
+              Just z  -> return z
 
 main :: IO()
 main = do
