cbc307227cd162f620b08803aaaa12dea6e83411
Titus Flamininus <mnyika@gmail.com>
2017-02-28 19:31:57 +0100

    first definitions of data

--- Haskell/Nurikabe/Nurikabe.hs
+++ Haskell/Nurikabe/Nurikabe.hs
@@ -1,1 +1,10 @@
 --Nurikabe : https://www.brainbashers.com/nurikabehelp.asp
+
+--the kind of cell it is
+data Cellkind = Island | Water deriving (Show)
+
+--complete description of a single cell on the board
+data NuriCell = NuriCell { x::Int, y::Int, kind::Cellkind}
+
+main :: IO()
+main = return ()
