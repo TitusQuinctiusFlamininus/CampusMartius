f8d6ed2e07bbe0bcc9e8f1737aed9da3556ece2f
Titus Flamininus <mnyika@gmail.com>
2017-03-01 21:26:01 +0100

    COOL

--- Haskell/Nurikabe/Nurikabe.hs
+++ Haskell/Nurikabe/Nurikabe.hs
@@ -1,10 +1,11 @@
 --Nurikabe : https://www.brainbashers.com/nurikabehelp.asp
+import Data.Char
 
 --the kind of cell it is
 data Cellkind = Island | Water deriving (Eq, Show)
 
 --complete description of a single cell on the board
-data NuriCell = NuriCell { locX::Int, locY::Int, size::Int, kind::Cellkind} deriving (Eq, Show)
+data NuriCell = NuriCell { locX::Int, locY::Int, size::Int, kind::Cellkind } deriving (Eq, Show)
 
 --function to generate the board
 createNuriBoard :: [NuriCell] -> [NuriCell]
@@ -15,6 +16,32 @@ createNuriBoard board
          noRegionBoard = map (\x -> NuriCell {locX = x, locY=y, size=0, kind=Island}) [1..9] in
          createNuriBoard (board ++ noRegionBoard)
 
+--function to update a board with the default nurikabe values
+setDefaultNurikabeValues :: [(Int, Int, Int)] -> [NuriCell] -> [NuriCell]
+setDefaultNurikabeValues [] rest = rest
+setDefaultNurikabeValues def@((a,b,c):ys) (NuriCell {locX=x, locY=y, size=z, kind=g}:xs)
+ | a == x && b == y   = (NuriCell {locX=x, locY=y, size=c, kind=g}) : setDefaultNurikabeValues ys xs
+ | otherwise          = (NuriCell {locX=x, locY=y, size=z, kind=g}) : setDefaultNurikabeValues def xs
 
-main :: IO()
-main = putStr (show (createNuriBoard []))
+
+--function to convert string input for defaut cell values to a format we know about
+inputToDefault :: String -> [(Int, Int, Int)]
+inputToDefault ""  = []
+inputToDefault " " = []
+inputToDefault (x:y:z:xs)
+ | x == ',' = inputToDefault (y:z:xs)
+ | otherwise = ((digitToInt x),(digitToInt y),(digitToInt z)) : inputToDefault xs
+
+main :: IO ()
+main = do
+ putStrLn "***********************************************************************************************************"
+ putStrLn "===================================WELCOME TO NURIKABE (from Brainbashers)==============================================="
+ putStrLn "***********************************************************************************************************"
+ putStrLn ""
+ putStrLn "Enter values already solved on the board in the format: 123,456 etc...."
+ putStrLn " For example: 123,456 would imply: 2nd cell in 1st column has value 3, 5th cell in the 4th column has value 6, etc "
+ putStrLn "(Note: Traverse the board from lower left cell, moving left to right for the bottom row, then the next row, etc....)"
+ inputValues <- getLine
+ let hollowboard = createNuriBoard []
+     defaultInput = inputToDefault inputValues in
+     putStr (show (setDefaultNurikabeValues defaultInput hollowboard))
