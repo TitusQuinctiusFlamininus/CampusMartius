46243a9e075d78622816be843e9046e0ca96cdd3
Titus Flamininus <mnyika@gmail.com>
2017-03-09 21:20:35 +0100

    ok this works

--- Haskell/Nurikabe/Nurikabe.hs
+++ Haskell/Nurikabe/Nurikabe.hs
@@ -74,8 +74,6 @@ groupAllUniverses  (b@NuriCell{locX=_, locY=_, size=s, kind=_}:bs) (x:xs) =
 
 --function to remove any lists that are empty in the list of lists
 cleanGroupedUniverses :: [NuriCell] -> [[[NuriCell]]] -> [[[NuriCell]]]
-cleanGroupedUniverses baselist ([]) = [[[]]]
-cleanGroupedUniverses baselist ((single@(a:[])):as) = [[a]] ++ cleanGroupedUniverses baselist as
 cleanGroupedUniverses baselist grpUnis =
   let cleaned = nub $ map(\b -> homeToMama (concat grpUnis) b) baselist
       in map (\grp -> if (head grp) == [] then drop 1 grp else grp ) cleaned
@@ -99,10 +97,8 @@ checkIfNeighboursBelong (p:ps) brd =
       singleposstruth : checkIfNeighboursBelong ps brd
 
 --Function to get all the possible wide range of bridges and narrow it down to the the list that could only be real bridges
-findAllBridges :: [NuriCell] -> [[[NuriCell]]] -> [NuriCell] -> [[[NuriCell]]]
-findAllBridges baselist poss brd =
-   map (\w -> filter (\x -> all (==True) (checkIfNeighboursBelong x brd)) w )  poss
-  --in map (\u -> [[u]]) (filter (\b@NuriCell{locX=_, locY=_, size=s, kind=_} -> s==1) baselist) ++ longislands
+findAllBridges :: [[[NuriCell]]] -> [NuriCell] -> [[[NuriCell]]]
+findAllBridges poss brd = map (\w -> filter (\x -> all (==True) (checkIfNeighboursBelong x brd)) w )  poss
 
 
 main :: IO ()
@@ -123,6 +119,6 @@ main = do
      gathereduniverses = gatherAllUniverses baseislandlist readyboard
      groupeduniverses = groupAllUniverses baseislandlist gathereduniverses
      cleaneduniverses = cleanGroupedUniverses baseislandlist groupeduniverses
-     trueislandlist = findAllBridges baseislandlist cleaneduniverses readyboard
-    in putStrLn (show (groupeduniverses)++(show (length groupeduniverses)))
+     trueislandlist = findAllBridges cleaneduniverses readyboard
+    in putStrLn (show (trueislandlist)++(show (length trueislandlist)))
     --in putStrLn (show (results))
