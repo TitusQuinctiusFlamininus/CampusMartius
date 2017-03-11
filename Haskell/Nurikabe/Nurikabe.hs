0fa17fbbf315112d5b377ba4c1c33cf13b84e5dc
Titus Flamininus <mnyika@gmail.com>
2017-03-11 13:58:29 +0100

    much better, we can know if any of the 4x4 combinations are a block of water, and if any of them turns out to be true, then we have a water problem

--- Haskell/Nurikabe/Nurikabe.hs
+++ Haskell/Nurikabe/Nurikabe.hs
@@ -108,7 +108,8 @@ doesWaterBlockExist cell@NuriCell{locX=x, locY=y, size=_, kind=k} brd =
                    [cell, NuriCell{locX=x-1, locY=y, size=0, kind=k}, NuriCell{locX=x, locY=y+1, size=0, kind=k},NuriCell{locX=x-1, locY=y+1, size=0, kind=k}],
                    [cell, NuriCell{locX=x+1, locY=y, size=0, kind=k}, NuriCell{locX=x, locY=y+1, size=0, kind=k},NuriCell{locX=x+1, locY=y+1, size=0, kind=k}]
                   ]
-  in any (==True)(concat $ map (\poss -> map (\p -> p `elem` brd) poss) posscells)
+      indvtruths = map (\poss -> map (\p -> p `elem` brd) poss) posscells in
+      any (==True) $ map (\tlist -> all (==True) tlist) indvtruths
 
 main :: IO ()
 main = do
