770ce9d5b83450ba5babbf6446470df050b950d7
Michael Nyika <michael.nyika@king.com>
2017-05-04 20:54:33 +0200

    making the older examples clearer

--- Haskell/Exercises/ConfigurationRead_ReaderMonad.hs
+++ Haskell/Exercises/ConfigurationRead_ReaderMonad.hs
@@ -2,21 +2,22 @@ import Control.Monad.Trans.Reader
 import Data.Map as M
 
 
-type Bindings = Map String Int;
+type Bindings = Map String Int; -- how our configuration binding will be composed
+type Mydatabase a = Reader Bindings a --the reader monad we will use to hold the binding
 
-type Mydatabase a = Reader Bindings a
-
+--typical configuration data
 myDb = [("val1", 34), ("val2", 89),("val3", 4),("val4", 1007),("val5", 678)]
 
+--function that takes a key and returns its value from the configuration, otherwise it returns zero
 process :: String -> Mydatabase Int
 process key = do
-           env <- ask
-           let lookedup = (M.lookup key env) in
-            case lookedup of
-              Nothing -> return 0
-              Just z  -> return z
+                env <- ask
+                case (M.lookup key env) of
+                  Just z    -> return z
+                  otherwise -> return 0
 
+--main function
 main :: IO()
 main = do
-         let finale = runReader (process "val4") (M.fromList myDb) in
-             putStrLn (show finale)
+         finale <- return $ runReader (process "val4") (M.fromList myDb)
+         putStrLn (show finale)
