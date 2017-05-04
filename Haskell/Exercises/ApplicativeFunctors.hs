74e10a6355cd8be5b6a58fca4455f62373371a13
Michael Nyika <michael.nyika@king.com>
2017-05-04 11:13:19 +0200

    just to see what is going on

--- Haskell/Exercises/ApplicativeFunctors.hs
+++ Haskell/Exercises/ApplicativeFunctors.hs
@@ -8,22 +8,25 @@ instance Functor AT where
 --applicative instance of AT
 instance Applicative AT where
   pure = L
-  (L f) <*> b = fmap f b
-  f <*> (L b) = fmap ($ b) f
+  (L f) <*> b   = fmap f b
+  f <*> (L b)   = fmap ($ b) f
   B f1 f2 <*> b = B (f1 <*> b) (f2 <*> b)
 
 --instance monad of AT
 instance Monad AT where
   return = L
-  (L b) >>= f = f b
+  (L b) >>= f   = f b
   (B l r) >>= f = B (l >>= f) (r >>= f)
 
 somefunction :: AT String
-somefunction = do L "on" >>= (\x -> let r = "so far "++x in
-                                        B (L r) (L (" it goes on"++ (concat $ (replicate 4 x)))))
-                         >>= (\y -> let f = "and finally "++y in
-                                        B (L (f++"man i am done")) (L (" coool"++ (concat $ (replicate 7 f)))))
-              
+somefunction = do L "on" >>= (\x -> let r = "so far <"++x++">" in
+                                        B (L r) (L " it goes on")
+                                        --B (L r) (L (" it goes on"++ (concat $ (replicate 4 x)))))
+                         >>= (\y -> let f = "and finally <"++y++">" in
+                                        B (L (f++"man i am done")) (L " coool")))
+
+                                        --B (L (f++"man i am done")) (L (" coool"++ (concat $ (replicate 7 f)))))
+
 
 main :: IO ()
 main = do
