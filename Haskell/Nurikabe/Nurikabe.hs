c1641dc6849bd114ac81cd0c9354c78979c0f7aa
Titus Flamininus <mnyika@gmail.com>
2017-06-11 22:31:08 +0200

    formatting

--- Haskell/Nurikabe/Nurikabe.hs
+++ Haskell/Nurikabe/Nurikabe.hs
@@ -1,41 +1,41 @@
 --Nurikabe : https://www.brainbashers.com/nurikabehelp.asp
 import Data.Char
 import Data.List
-import Data.Maybe(fromJust)
-import Control.Monad.Trans.State.Lazy(StateT, put, get, execStateT)
-import Control.Monad.Trans.Reader(ReaderT, runReaderT, ask)
-import Control.Monad.Trans.Class(lift)
-import Control.Monad.IO.Class(liftIO)
+import Data.Maybe                           (fromJust)
+import Control.Monad.Trans.State.Lazy       (StateT, put, get, execStateT)
+import Control.Monad.Trans.Reader           (ReaderT, runReaderT, ask)
+import Control.Monad.Trans.Class            (lift)
+import Control.Monad.IO.Class               (liftIO)
 
-data Cellkind = Island | Water deriving (Eq, Show) --the kind of cell it is
-data NuriCell = NuriCell { locX::Int, locY::Int, size::Int, kind::Cellkind } deriving (Eq, Show) --complete description of a single cell on the board
-type AllIslands = [[[NuriCell]]] --list of all island possibilities for all user inputToDefault
-type Strategy   = [(Int,Int)]    -- Tuple list : First Int = the index of Island in AllIsland ; Second Int = Index of list to choose within the list of islands
-type Nuriboard  = [NuriCell]     -- a typical Nurikabe board
-type BaseIslandList = [NuriCell] -- Just a list of Nuricells that the user set as default
-type Nurikabe a = ReaderT AllIslands (StateT (Strategy,[NuriCell]) IO) a  --the monad stack that we will use to solve Nurikabe
+data Cellkind       =     Island | Water deriving (Eq, Show) --the kind of cell it is
+data NuriCell       =     NuriCell { locX::Int, locY::Int, size::Int, kind::Cellkind } deriving (Eq, Show) --complete description of a single cell on the board
+type AllIslands     =     [[[NuriCell]]] --list of all island possibilities for all user inputToDefault
+type Strategy       =     [(Int,Int)]    -- Tuple list : First Int = the index of Island in AllIsland ; Second Int = Index of list to choose within the list of islands
+type Nuriboard      =     [NuriCell]     -- a typical Nurikabe board
+type BaseIslandList =     [NuriCell] -- Just a list of Nuricells that the user set as default
+type Nurikabe a     =     ReaderT AllIslands (StateT (Strategy,[NuriCell]) IO) a  --the monad stack that we will use to solve Nurikabe
 
 --function to generate a Nurikabe 9x9 board full of Water cells
 createNuriBoard :: Nuriboard -> Nuriboard
 createNuriBoard board
- | length board == 81     = board
+ | length board == 81       = board
  | otherwise                =
-     let y = if (null board) then 1 else ((length board) `div` 9)+1
-         noRegionBoard = map (\x -> NuriCell {locX = x, locY=y, size=0, kind=Water}) [1..9] in
+     let y                  = if (null board) then 1 else ((length board) `div` 9)+1
+         noRegionBoard      = map (\x -> NuriCell {locX = x, locY=y, size=0, kind=Water}) [1..9] in
          createNuriBoard (board ++ noRegionBoard)
 
 --function to update a board with the default nurikabe values
 setDefaultIslands :: [(Int, Int, Int)] -> Nuriboard -> Nuriboard
 setDefaultIslands [] rest = rest
 setDefaultIslands def@((a,b,c):ys) (cell@(NuriCell {locX=x, locY=y, size=z, kind=g}):xs)
- | a == x && b == y   = (NuriCell {locX=x, locY=y, size=c, kind=Island}) : setDefaultIslands ys xs
- | otherwise          = cell : setDefaultIslands def xs
+ | a == x && b == y       = (NuriCell {locX=x, locY=y, size=c, kind=Island}) : setDefaultIslands ys xs
+ | otherwise              = cell : setDefaultIslands def xs
 
 
 --function to convert string input for defaut cell values to a format we know about
 inputToDefault :: String -> [(Int, Int, Int)]
-inputToDefault ""  = []
-inputToDefault " " = []
+inputToDefault ""                   = []
+inputToDefault " "                  = []
 inputToDefault (x:y:z:xs)
  | x == ',' || y == ',' || z == ',' = inputToDefault (y:z:xs)
  | otherwise = ((digitToInt x),(digitToInt y),(digitToInt z)) : inputToDefault xs
@@ -47,7 +47,7 @@ createBaseIslandList  = filter (\NuriCell{locX=_, locY=_, size=r, kind=_} -> r >
 --function to give a list of all cells in its vicinity that could qualify as a part of an island formed when the given cell is one of the island cells
 -- A cell, the Nuriboard and how many cells the island will be composed of
 findCellUniverse :: NuriCell -> Nuriboard -> Int -> [NuriCell]
-findCellUniverse _ _ 0 = []
+findCellUniverse _ _ 0                                               = []
 findCellUniverse cell@NuriCell{locX=x, locY=y, size=s, kind=_} brd n =
  let maxdist = n-1 in
      [cell] ++ (filter (\NuriCell{locX=a, locY=b, size=_, kind=_} -> ((a <= (x+maxdist)) &&
@@ -61,7 +61,7 @@ groupPoss :: Int -> [NuriCell] -> [[NuriCell]]
 groupPoss 0 _ = [[]]
 groupPoss n xs =   do
                     y:xs' <- tails xs
-                    ys <- groupPoss (n-1) xs'
+                    ys    <- groupPoss (n-1) xs'
                     return (y:ys)
 
 --function to give the list of all lists of possibilities cells that could be islands
@@ -69,13 +69,13 @@ gatherAllUniverses :: [NuriCell] -> Nuriboard -> [[NuriCell]]
 gatherAllUniverses [] _  =  [[]]
 gatherAllUniverses (b@NuriCell{locX=_, locY=_, size=s, kind=_}:bs) brd =
   let gathered = findCellUniverse b brd s : gatherAllUniverses bs brd in
-  filter (\f -> f /= []) gathered
+  filter (/= []) gathered
 
 --function to group all the cell into universes, using the baselist as data
 groupAllUniverses :: [NuriCell] -> [[NuriCell]] -> AllIslands
 groupAllUniverses [] _ = [[[]]]
 groupAllUniverses  (NuriCell{locX=_, locY=_, size=s, kind=_}:bs) (x:xs) = let grouped = groupPoss s x : groupAllUniverses bs xs
-       in filter (\f -> f /= [[]]) grouped
+       in filter (/= [[]]) grouped
 
 --function to remove any lists that are empty in the list of lists
 cleanGroupedUniverses :: BaseIslandList -> AllIslands -> AllIslands
@@ -116,9 +116,9 @@ findAllBridges poss brd =
 --Arg 1 = Each list represents a DIFFERENT ISLAND, so this list is, for example, all the first lists of each [[[NuriCell]]]
 --When all individual checks are True, then we will have a result of TRUE (since we are ANDing && many true results)
 checkNoIslandOverlapOrAdj :: [[NuriCell]] -> Nuriboard -> Bool
-checkNoIslandOverlapOrAdj ([]:_) _ = True
-checkNoIslandOverlapOrAdj (_:[]) _  = True
-checkNoIslandOverlapOrAdj ((a:as):bs) brd=
+checkNoIslandOverlapOrAdj ([]:_) _         = True
+checkNoIslandOverlapOrAdj (_:[]) _         = True
+checkNoIslandOverlapOrAdj ((a:as):bs) brd  =
   let neighs              = findNeighours a brd
       singlecellcheck     =  all (==False) $ ((map (\other -> a `elem` other) bs) ++ (map (`elem` neighs) (concat bs)))
       fullislandcheck     = singlecellcheck && checkNoIslandOverlapOrAdj (as:bs) brd
@@ -189,15 +189,15 @@ prepNuri baseislandlist readyboard =
 checkNuri :: Nurikabe ()
 checkNuri = do
       trueislandlist         <-  ask
-      (strategy,readyboard)  <-  lift $ get
+      (strategy,readyboard)  <-  lift get
       let islandcombination   =  makeAllCellsIslands $ findNextIslandCombination trueislandlist strategy
           groundedboard        = setBoardPossibility readyboard (concat islandcombination)
           nooverlaps          = checkNoIslandOverlapOrAdj islandcombination readyboard
           nobadwater          = all (==False) (map (\cell -> doesWaterBlockExist cell groundedboard) (filter (\f -> kind f == Water) groundedboard)) in
           do
-            liftIO $ putStrLn ("strategy: "++show (strategy))
-            liftIO $ putStrLn ("nooverlaps: "++show (nooverlaps))
-            liftIO $ putStrLn ("nobadwater: "++show (nobadwater))
+            liftIO $ putStrLn $ "strategy: "++show (strategy)
+            liftIO $ putStrLn $ "nooverlaps: "++show (nooverlaps)
+            liftIO $ putStrLn $ "nobadwater: "++show (nobadwater)
             if (nooverlaps && nobadwater)
             then do lift $ put (strategy,groundedboard)
             else let nexstrat = findNextIslandStrategy strategy in
@@ -216,13 +216,13 @@ main = do
  putStrLn "Enter values already solved on the board in the format: 123,456 etc...."
  putStrLn " For example: 123,456 would imply: 2nd cell in 1st column is an island of length 3, 5th cell in the 4th column is an island of length 6, etc "
  putStrLn "(Note: Traverse the board from lower left cell, moving left to right for the bottom row, then the next row, etc....)"
- inputValues <- getLine
- let hollowboard = createNuriBoard []
-     defaultInput = inputToDefault inputValues
-     readyboard = setDefaultIslands defaultInput hollowboard
+ inputValues        <- getLine
+ let hollowboard    = createNuriBoard []
+     defaultInput   = inputToDefault inputValues
+     readyboard     = setDefaultIslands defaultInput hollowboard
      baseislandlist = createBaseIslandList readyboard
      trueislandlist = prepNuri baseislandlist readyboard
-     strategy = constructIslandStrategy trueislandlist in
+     strategy       = constructIslandStrategy trueislandlist in
      do
       (finalstrat,finalNurikabeSolution) <- execStateT (runReaderT checkNuri trueislandlist) (strategy,readyboard)
       if finalNurikabeSolution == [] then putStrLn "There was No Nurikabe Solution Found! (Recheck your islands...)"
@@ -230,4 +230,4 @@ main = do
          putStrLn "!§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§"
          putStrLn "!! WE FOUND A NURIKABE SOLUTION!!! "
          putStrLn "!§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§"
-         putStrLn (show (finalNurikabeSolution)++(show (length finalNurikabeSolution)))
+         putStrLn $ show (finalNurikabeSolution)++(show $ length finalNurikabeSolution)
