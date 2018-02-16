ad64585c1cc9f3d97c956d953ffd503dbfbe5295
michael.nyika <michael.nyika@king.com>
2018-02-16 20:32:56 +0100

    documentation

--- Haskell/Exercises/MyFirstContravariantFunctor.hs
+++ Haskell/Exercises/MyFirstContravariantFunctor.hs
@@ -7,7 +7,7 @@ data MyContraThing a b = MyContraThing { getIt :: (b -> a) }
 instance Contravariant (MyContraThing a) where
  contramap f (MyContraThing c)      =   MyContraThing (c . f) -- the input (6) will be applied to f first, not c !
 --also note: the function f must take (as input), things of the same type, as the function c
---that is: if c takes in Ints, then f must take Ints as well
+--that is: if c takes in Ints, then f must take Ints as well    
 
 doSomething :: MyContraThing [String] Int
 doSomething = contramap (*2)  MyContraThing { getIt = (\i -> replicate i "here") }
