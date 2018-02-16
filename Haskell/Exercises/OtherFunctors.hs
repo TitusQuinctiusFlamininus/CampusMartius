fd4d615c335693f8ddb52f9b3dc9e61b10c82ab2
michael.nyika <michael.nyika@king.com>
2018-02-16 18:28:23 +0100

    it is much clearer now

--- Haskell/Exercises/OtherFunctors.hs
+++ Haskell/Exercises/OtherFunctors.hs
@@ -4,24 +4,14 @@ import Data.Functor.Contravariant
 
 data MyContraThing a b = MyContraThing { getIt :: (b -> a) }
 
-
 instance Contravariant (MyContraThing a) where
  contramap f (MyContraThing c)      =   MyContraThing (c . f) 
 
-somefunc :: Int -> [String]
-somefunc input =  replicate input "here" 
-
-mything = MyContraThing { getIt = somefunc }
-
-producer :: Int -> Int
-producer = (*2)
-
 doSomething :: MyContraThing [String] Int
-doSomething = contramap producer mything
-
+doSomething = contramap (*2)  MyContraThing { getIt = (\i -> replicate i "here") }
 
 runIt :: (MyContraThing [String] Int) -> Int -> [String]
 runIt (MyContraThing f) = f
 
 main :: IO ()
-main = mapM_ putStrLn $ runIt mything 6
+main = mapM_ putStrLn $ runIt doSomething 6
