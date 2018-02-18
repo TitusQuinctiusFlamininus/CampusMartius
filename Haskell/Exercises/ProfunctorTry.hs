887ba3b1adfa68d07f7ab1167733b45b8bb593d1
michael.nyika <michael.nyika@king.com>
2018-02-18 11:40:17 +0100

    Finally a simple Profunctor

---
+++ Haskell/Exercises/ProfunctorTry.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE ExistentialQuantification #-}
+
+module ProfunctorTry where
+
+import Data.Profunctor 
+
+
+data L a b   = forall s . L (s -> b) (a -> s)
+
+
+instance Profunctor L where
+ dimap h g (L result spin) = L (g. result) (spin . h)
+ 
+ 
+runTheP :: L Int String -> Int -> String
+runTheP (L result spin) input  = result (spin input)
+
+
+spinner :: Int -> [String]
+spinner i = replicate i $ "thetruth"
+
+resulter :: [String] -> String
+resulter = concat 
+
+main = runTheP (L resulter spinner) 100
