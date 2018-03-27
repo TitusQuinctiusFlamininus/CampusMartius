de6939fca79daa348d548c60bbcdf5f5e3c82154
michael.nyika <michael.nyika@king.com>
2018-03-27 17:26:57 +0200

    little adjustments

--- Haskell/Exercises/Continuations_Example.hs
+++ Haskell/Exercises/Continuations_Example.hs
@@ -1,20 +1,22 @@
+
 --functions to illustrate the use of continuations
 --
 --
 --this function will create continuations from a list, given a fulcrum
+
+
 createContinuation :: (Num a, Ord a) => a -> [a] -> [(a -> a)]
 createContinuation  _     [] = []
 createContinuation  fulc inp = map ((*)) . filter (< fulc) $ inp
 
 --this function will create a suspended computation from a single input
-suspend :: (Num a, Ord a) => a  -> ((a -> a) -> a)
-suspend x            = ($ x)
+suspend :: a  -> ((a -> a) -> a)
+suspend x      = ($ x)
 
 main :: IO ()
-main                 = do
-  let answer         = map (suspend 34 ) . createContinuation 5 $ [1,2,1,2,0,2,4,5,2,4,6,1,0]
-   in putStrLn $ show answer
-
+main           = putStrLn $ show answer
+                 where answer = map (suspend 34 ) . createContinuation 5 $ [1,2,1,2,0,2,4,5,2,4,6,1,0]
+                
 
 
 
