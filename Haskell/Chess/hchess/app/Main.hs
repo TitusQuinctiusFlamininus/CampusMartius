0c61effb7b3105f21a98af2ce378efc77c5d8339
michael.nyika <michael.nyika@king.com>
2018-04-14 23:48:10 +0200

    This should not have compiled, but it did so we need to fix this with typefamilies

--- Haskell/Chess/hchess/app/Main.hs
+++ Haskell/Chess/hchess/app/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
 import Datatypes.ChessPlay
+import Datatypes.ChessPieces
+
+
+raisePiece :: Piece
+raisePiece = let king = Piece {name=KING, color=BLACK, worth=1000, location=(5,8)} in 
+    promote king BISHOP (6,1)
 
 main :: IO ()
-main = undefined
+main = putStrLn $ show raisePiece
