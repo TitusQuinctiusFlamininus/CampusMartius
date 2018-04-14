856535a73766a75f307698af872f192b5bbaeffe
michael.nyika <michael.nyika@king.com>
2018-04-14 12:24:18 +0200

    Defined how all pieces can capture and move around the board

--- Haskell/Chess/hchess/src/Datatypes/ChessPlay.hs
+++ Haskell/Chess/hchess/src/Datatypes/ChessPlay.hs
@@ -9,6 +9,12 @@ type Board = [Location]
 --typeclass embodying the ability of a piece to translocate from one square to another
 --either to capture or simply to move
 class Movable p where
+    --a piece to move and its intended location
     move    :: p -> Location -> p
-    capture :: p -> p -> (p, [p])
-    
+    -- a killer, a victim, a list of all victims captured so far
+    capture :: p -> p -> [p] -> (p, [p])
+
+
+instance Movable Piece where
+    move p@_  (x,y) = p { location = (x,y)} 
+    capture k v l   = (k {location = location v}, (v:l))
