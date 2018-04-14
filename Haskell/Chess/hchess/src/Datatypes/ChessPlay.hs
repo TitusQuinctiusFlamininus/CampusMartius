a1fd1cf1ca74f80f2cd21d8619c5e6e2693312a1
michael.nyika <michael.nyika@king.com>
2018-04-14 20:31:52 +0200

    ok, so we can promote one piece into another piece

--- Haskell/Chess/hchess/src/Datatypes/ChessPlay.hs
+++ Haskell/Chess/hchess/src/Datatypes/ChessPlay.hs
@@ -1,3 +1,5 @@
+
+
 module Datatypes.ChessPlay where
 
 import Datatypes.ChessPieces
@@ -14,7 +16,16 @@ class Movable p where
     -- a killer, a victim, a list of all victims captured so far
     capture :: p -> p -> [p] -> (p, [p])
 
-
+--lets make all our pieces movable and the ability to capture other pieces
 instance Movable Piece where
     move p@_  (x,y) = p { location = (x,y)} 
     capture k v l   = (k {location = location v}, (v:l))
+
+--pawns can be promoted to other pieces
+class Promotable p where
+    promote :: p -> PieceType -> Location -> p
+    
+
+--lets make pawns promotable
+instance Promotable Piece where
+    promote p t l = p {name = t, location = l} 
