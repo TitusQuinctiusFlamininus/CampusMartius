22cfe47c43597bfcfbb625562c0ede97444c1a08
michael.nyika <michael.nyika@king.com>
2018-04-16 11:25:34 +0200

    dont need it

--- Haskell/Chess/hchess/src/Datatypes/ChessPlay.hs
+++ Haskell/Chess/hchess/src/Datatypes/ChessPlay.hs
@@ -5,10 +5,6 @@ module Datatypes.ChessPlay where
 
 import Datatypes.ChessPieces
 
---the gameboard, as a list of coordinates on an 8x8 
-type Board = [Location]
-
-
 --typeclass embodying the ability of a piece to translocate from one square to another
 --either to capture or simply to move
 class Movable p where
