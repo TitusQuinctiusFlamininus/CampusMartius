24ee7b7933cabfbe1926f93d13d5ca05fea84aff
michael.nyika <michael.nyika@king.com>
2018-04-16 12:31:10 +0200

    doc

--- Haskell/Chess/hchess/src/Datatypes/ChessPlay.hs
+++ Haskell/Chess/hchess/src/Datatypes/ChessPlay.hs
@@ -7,10 +7,10 @@ import Datatypes.ChessPieces
 
 --typeclass embodying the ability of a piece to translocate from one square to another
 --either to capture or simply to move
+--for move:  --a piece to move and its intended location
+--for capture:  -- a killer, a victim, a list of all victims captured so far
 class Movable p where
-    --a piece to move and its intended location
     move    :: p -> Location -> p
-    -- a killer, a victim, a list of all victims captured so far
     capture :: p -> p -> [p] -> (p, [p])
 
 --lets make all our pieces movable and the ability to capture other pieces
@@ -21,7 +21,8 @@ instance Movable (Piece a)where
 --typeclass representing minor pieces (i.e pawns). Major pieces are any pieces that are NOT pawns, since we c
 class Promotable p t where
     promote :: p -> t -> Piece a
-    
+
+--promoting pawns to different major pieces
 instance (Minor a, Major b) => Promotable (Piece a) (Piece b) where
     promote p@Piece {name=PAWN} r@Piece{name=ROOK}   = 
      Piece {name=ROOK, color=(color p), worth=(worth r),   location=(fst (location p), (if (color p == BLACK) then 1 else 8))}
