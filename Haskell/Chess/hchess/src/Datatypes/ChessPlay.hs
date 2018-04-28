de4d60958585216384c55d917e165a2dc26665b0
michael.nyika <michael.nyika@king.com>
2018-04-28 12:20:54 +0200

    Knight moves can be determined, but not considering location of the piece
    we need to use the location of the piece we are given

--- Haskell/Chess/hchess/src/Datatypes/ChessPlay.hs
+++ Haskell/Chess/hchess/src/Datatypes/ChessPlay.hs
@@ -4,6 +4,11 @@ module Datatypes.ChessPlay where
 
 import Datatypes.ChessTypes
 
+--function to take any list, duplicate it, negate all values in the duplicate and concatenate to the original list
+dupNegateHalf :: (Num a) => [a] -> [a]
+dupNegateHalf o = o ++ map negate o
+              
+
 --determine unmodified (raw) possible moves of a piece, based on its current position 
 mPossibility :: Piece a -> Moves [Location]
-mPossibility (name  -> KNIGHT) = Moves $ zipWith (\f r -> (f, r)) [2,2,1,1,-2,-2,-1,-1] [1,-1,2,-2,1,-1,2,-2]
+mPossibility (name -> KNIGHT) = Moves $ zipWith (\f r -> (f,r)) (dupNegateHalf [2,2,1,1]) (concat . (replicate 2) $ [1,-1,2,-2])
