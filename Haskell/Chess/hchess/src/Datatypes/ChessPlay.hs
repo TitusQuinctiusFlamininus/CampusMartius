cfdccc02746cfcb1694adc6dbfcffafb930a4f6a
michael.nyika <michael.nyika@king.com>
2018-04-28 13:38:09 +0200

    dealing with a monad here

--- Haskell/Chess/hchess/src/Datatypes/ChessPlay.hs
+++ Haskell/Chess/hchess/src/Datatypes/ChessPlay.hs
@@ -6,8 +6,8 @@ import Utilities.ChessUtils
 
 --determine unmodified (raw) possible moves of a piece, based on its current position 
 mPossibility :: Piece a -> Moves [Location]
-mPossibility p@Piece{name=KNIGHT} = Moves $ zipWith (\f r -> (f,r)) fileList rankList
-                                    where fileList = [(fl+2), (fl+2), (fl+1), (fl+1),(fl-2), (fl-2), (fl-1), (fl-1)]
-                                          rankList = concat . replicate 2 $ [(rk+1), (rk-1), (rk+2), (rk-2)]
+mPossibility p@Piece{name=KNIGHT} = return $ zipWith (\f r -> (f,r)) fileList rankList
+                                    where fileList = [(fl+2), (fl+2), (fl+1), (fl+1), (fl-2), (fl-2), (fl-1), (fl-1)]
+                                          rankList = concat . replicate 2 $  [(rk+1), (rk-1), (rk+2), (rk-2)]
                                           fl = cFile p
                                           rk = cRank p
