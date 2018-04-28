ccbdab7d3f5102ce004e61eb0aba530bd08b7c10
michael.nyika <michael.nyika@king.com>
2018-04-28 15:52:02 +0200

    We can get all locations of all black pieces or all white pieces on the board, as a list

--- Haskell/Chess/hchess/src/Datatypes/ChessPlay.hs
+++ Haskell/Chess/hchess/src/Datatypes/ChessPlay.hs
@@ -15,3 +15,8 @@ mPossibility p@Piece{name=KNIGHT} = return $ zipWith (\f r -> (f,r)) fileList ra
 --filter out all locations outside the board, given as locations in the list 
 filterNoBoard :: [Location] -> Moves [Location]
 filterNoBoard l = return $ outerRanks . outerFiles $ l 
+
+--function to filter out all locations that have already been occupied one's own color pieces 
+--for example, if black wants to move, then he can only do so to a sqaure not occupied by his own pieces
+filterOwnOccupied :: [Location] -> Moves [Location]
+filterOwnOccupied l = undefined
--- Haskell/Chess/hchess/src/Utilities/ChessUtils.hs
+++ Haskell/Chess/hchess/src/Utilities/ChessUtils.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ViewPatterns #-}
+
 module Utilities.ChessUtils where
 
 import Datatypes.ChessTypes
@@ -18,4 +20,18 @@ outerRanks = filter (\k -> snd k >= 1) . filter (\k -> snd k <= 8)
 --function to filter out all all bad files (files that are outside the board)
 outerFiles :: [Location] -> [Location]
 outerFiles = filter (\k -> fst k >= 1) . filter (\k -> fst k <= 8)
-                           
+                
+--function to obtain all locations for black pieces                 
+allColorLocations :: [BoardPiece] -> Color -> [Location]
+allColorLocations [] _  = []
+allColorLocations (x:xs) c =  if (getColor x == c) then (getLocation x : allColorLocations xs c) else allColorLocations xs c
+
+getColor :: BoardPiece -> Color
+getColor (K p)  = color p
+getColor (MI p) = color p
+getColor (MA p) = color p
+
+getLocation :: BoardPiece -> Location
+getLocation (K p)  = location p
+getLocation (MI p) = location p
+getLocation (MA p) = location p
