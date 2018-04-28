8ae21aebfcfe177e56a7c1a5281b7974830eaa23
michael.nyika <michael.nyika@king.com>
2018-04-28 13:00:51 +0200

    dont need this function

--- Haskell/Chess/hchess/src/Datatypes/ChessPlay.hs
+++ Haskell/Chess/hchess/src/Datatypes/ChessPlay.hs
@@ -4,11 +4,6 @@ module Datatypes.ChessPlay where
 import Datatypes.ChessTypes
 import Utilities.ChessUtils
 
---function to take any list, duplicate it, negate all values in the duplicate and concatenate to the original list
-dupNegateHalf :: (Num a) => [a] -> [a]
-dupNegateHalf o = o ++ map negate o
-              
-
 --determine unmodified (raw) possible moves of a piece, based on its current position 
 mPossibility :: Piece a -> Moves [Location]
 mPossibility p@Piece{name=KNIGHT} = Moves $ zipWith (\f r -> (f,r)) fileList rankList
