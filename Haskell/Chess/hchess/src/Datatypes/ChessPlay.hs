742133075c53459e4f0151d4e497b22c6d4f7284
michael.nyika <michael.nyika@king.com>
2018-04-29 20:44:48 +0200

    better function symbols

--- Haskell/Chess/hchess/src/Datatypes/ChessPlay.hs
+++ Haskell/Chess/hchess/src/Datatypes/ChessPlay.hs
@@ -14,22 +14,22 @@ mPossibility (f,r) t
  | t == BISHOP = return $ poss bFiles bRanks
  | otherwise   = return [] 
                  where poss   = zipWith locZipper 
-                       kFiles = (zipWith ($) ((<||>) 2 (+2) ++ (<||>) 2 (+1)) $ (<||>) 4 $ f) ++ (<||>) 2 (f-2) ++ (<||>) 2 (f-1)
-                       kRanks = concat . (<||>) 2 $ [(r+1), (r-1), (r+2), (r-2)]
-                       bFiles = concat . (<||>) 2 $ zipWith ($) ((<||>) uBound (+f)) boardSpan ++ zipWith ((-)) ((<||>) uBound f) boardSpan
-                       bRPos  = zipWith ($)   ((<||>) uBound (+r)) boardSpan 
-                       bRNeg  = zipWith ((-)) ((<||>) uBound r)    boardSpan
+                       kFiles = (zipWith ($) ((<->) 2 (+2) ++ (<->) 2 (+1)) $ (<->) 4 $ f) ++ (<->) 2 (f-2) ++ (<->) 2 (f-1)
+                       kRanks = concat . (<->) 2 $ [(r+1), (r-1), (r+2), (r-2)]
+                       bFiles = concat . (<->) 2 $ zipWith ($) ((<->) uBound (+f)) boardSpan ++ zipWith ((-)) ((<->) uBound f) boardSpan
+                       bRPos  = zipWith ($)   ((<->) uBound (+r)) boardSpan 
+                       bRNeg  = zipWith ((-)) ((<->) uBound r)    boardSpan
                        bRanks = bRPos ++ bRNeg ++ bRNeg ++ bRPos -- <- this is NOT a reversed list 
                                                            
 
 --filter out all locations outside the board, given as locations in the list 
 filterNoBoard :: [Location] -> Moves [Location]
-filterNoBoard l = return $ (\/) fst . (\/) snd $ l 
+filterNoBoard l = return $ (/|) fst . (/|) snd $ l 
 
 --function to filter out all locations that have already been occupied one's own color pieces 
 --for example, if black wants to move, then he can only do so to a sqaure not occupied by his own pieces
 --first parameter = from the Moves monad
 --second parameter = the list of all colored locations
 filterOwnOccupied :: [Location] -> [Location] -> Moves [Location]
-filterOwnOccupied l c = return $ (!>) l c
+filterOwnOccupied l c = return $ (>!<) l c
 
--- Haskell/Chess/hchess/src/Utilities/ChessUtils.hs
+++ Haskell/Chess/hchess/src/Utilities/ChessUtils.hs
@@ -4,21 +4,21 @@ import Datatypes.ChessTypes
 import Datatypes.ChessConstants
 
 --function to replicate some type 8 times
-(<||>) :: Int -> a -> [a]
-(<||>) = replicate 
+(<->) :: Int -> a -> [a]
+(<->) = replicate 
 
 --function to filter out all locations (or cells) that are completely outside the 8x8 board
-(\/) :: (Location -> RankOrFile) -> [Location] -> [Location]
-(\/) f = filter (\k -> f k >= lBound) . filter (\k -> f k <= uBound)
+(/|) :: (Location -> RankOrFile) -> [Location] -> [Location]
+(/|) f = filter (\k -> f k >= lBound) . filter (\k -> f k <= uBound)
 
 --function to check if any elements from one list appear in the second list; and return only those elements that
 --do not appear in the second list
-(!>) :: [Location] -> [Location] -> [Location]
-(!>)   []   _  = []
-(!>)   _    [] = []
-(!>) (x:xs) c  = case x `elem` c of 
-                        True  ->     (!>) xs c
-                        False -> x : (!>) xs c
+(>!<) :: [Location] -> [Location] -> [Location]
+(>!<)   []   _  = []
+(>!<)   _    [] = []
+(>!<) (x:xs) c  = case x `elem` c of 
+                        True  ->     (>!<) xs c
+                        False -> x : (>!<) xs c
 
 --function to obtain all locations for black pieces                 
 colouredLocations :: [BoardPiece] -> Color -> [Location]
