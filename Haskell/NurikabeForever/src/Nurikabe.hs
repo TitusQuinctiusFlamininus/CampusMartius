513b6bda637bcf980b248a0d79e5cf9730c8b107
michael.nyika <michael.nyika@king.com>
2018-06-29 10:57:46 +0200

    Looks good!

--- Haskell/NurikabeForever/src/Nurikabe.hs
+++ Haskell/NurikabeForever/src/Nurikabe.hs
@@ -1,3 +1,5 @@
+module Nurikabe where
+
 --Nurikabe : https://www.brainbashers.com/nurikabehelp.asp
 import Data.Char
 import Data.List
@@ -194,8 +196,8 @@ checkNuri = do
           nobadwater          = all (==False) $ map (\cell -> doesWaterBlockExist cell groundedboard) (filter (\f -> kind f == Water) groundedboard) in
           do
             liftIO $ putStrLn $ "strategy:   "   ++ show strategy
-            liftIO $ putStrLn $ "nooverlaps: "   ++ show nooverlaps
-            liftIO $ putStrLn $ "nobadwater: "   ++ show nobadwater
+            --liftIO $ putStrLn $ "nooverlaps: "   ++ show nooverlaps
+            --liftIO $ putStrLn $ "nobadwater: "   ++ show nobadwater
             if (nooverlaps && nobadwater)
             then do lift $ put (strategy,groundedboard)
             else let nexstrat = findNextIslandStrategy strategy in
@@ -205,24 +207,3 @@ checkNuri = do
               lift $ put (nexstrat,readyboard)
               checkNuri
 
-main :: IO ()
-main = do
- putStrLn "***********************************************************************************************************"
- putStrLn "===================================WELCOME TO NURIKABE (from Brainbashers)==============================================="
- putStrLn "***********************************************************************************************************"
- putStrLn ""
- putStrLn "Enter values already solved on the board in the format: 123,456 etc...."
- putStrLn " For example: 123,456 would imply: 2nd cell in 1st column is an island of length 3, 5th cell in the 4th column is an island of length 6, etc "
- putStrLn "(Note: Traverse the board from lower left cell, moving left to right for the bottom row, then the next row, etc....)"
- inputValues        <- getLine
- let readyboard     = setDefaultIslands [] (inputToDefault inputValues) $ createNuriBoard []
-     trueislandlist = prepNuri (createBaseIslandList readyboard) readyboard
-     strategy       = constructIslandStrategy trueislandlist in
-     do
-      (finalstrat,finalNurikabeSolution) <- execStateT (runReaderT checkNuri trueislandlist) (strategy,readyboard)
-      if finalNurikabeSolution == [] then putStrLn "There was No Nurikabe Solution Found! (Recheck your islands...)"
-      else do
-         putStrLn "!§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§"
-         putStrLn "!! WE FOUND A NURIKABE SOLUTION!!! "
-         putStrLn "!§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§"
-         putStrLn $ show (finalNurikabeSolution)++(show $ length finalNurikabeSolution)
