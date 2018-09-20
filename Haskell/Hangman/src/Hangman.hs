595d0c279979ffe1997d459fd1c22aa1ba2d6175
michael.nyika <michael.nyika@king.com>
2018-09-20 15:17:47 +0200

    i love viewpatterns

--- Haskell/Hangman/src/Hangman.hs
+++ Haskell/Hangman/src/Hangman.hs
@@ -1,21 +1,25 @@
+{-# LANGUAGE ViewPatterns #-}
+
 module Hangman where
     
 type UGuess = Char
 
 data HangWord    = HangWord { unhang   :: [Char],
                               idx      ::    Int,
-                              solution :: [Char]
-                            }
+                              solution :: [Char],
+                              chances  :: Int
+                            } deriving (Show)
 
 
 guessLetter :: UGuess -> HangWord -> HangWord
 guessLetter ' '   h  = h
 guessLetter g     h  = case safeRetr (solution h) (idx h) of 
-                        Nothing    -> h { idx = (idx h) + 1 }
-                        Just g     -> h { unhang = unhang h ++ [g], idx = (idx h) + 1 }
+                        Nothing    -> h { idx = (idx h) + 1, chances = (chances h) - 1 }
+                        Just g     -> h { unhang = g : unhang h, idx = (idx h) + 1, chances = (chances h) - 1 }
                         
                         
                         
 safeRetr :: [a] -> Int -> Maybe a
-safeRetr []   _     = Nothing
-safeRetr l    n     = if n > length l then Nothing else Just $ l !! n
+safeRetr []                   _         = Nothing
+safeRetr l    ((>) (length l)  -> True) = Nothing
+safeRetr l                    n         = Just $ l !! n 
