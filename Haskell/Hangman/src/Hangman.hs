e5ac255c2a0e8619b9890f9d5b4d7024d1ed8fa3
michael.nyika <michael.nyika@king.com>
2018-09-20 14:40:22 +0200

    ok

--- Haskell/Hangman/src/Hangman.hs
+++ Haskell/Hangman/src/Hangman.hs
@@ -3,11 +3,13 @@ module Hangman where
 type UGuess = Char
 
 data HangWord    = HangWord { unhang   :: [Char],
-                              cpos     ::    Int,
+                              idx      ::    Int,
                               solution :: [Char]
                             }
 
 
 guessLetter :: UGuess -> HangWord -> HangWord
 guessLetter ' '   h  = h
-guessLetter g     h  = undefined
+guessLetter g     h  = case solution h !! idx h of 
+                        g    -> h { unhang = unhang h ++ [g], idx = (idx h) + 1 }
+                        _    -> h { idx = (idx h) + 1 }
