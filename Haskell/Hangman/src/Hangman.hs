a9c766830682c7e243631ed2c006334739dab042
michael.nyika <michael.nyika@king.com>
2018-09-20 14:48:26 +0200

    ok

--- Haskell/Hangman/src/Hangman.hs
+++ Haskell/Hangman/src/Hangman.hs
@@ -10,6 +10,12 @@ data HangWord    = HangWord { unhang   :: [Char],
 
 guessLetter :: UGuess -> HangWord -> HangWord
 guessLetter ' '   h  = h
-guessLetter g     h  = case solution h !! idx h of 
-                        g    -> h { unhang = unhang h ++ [g], idx = (idx h) + 1 }
-                        _    -> h { idx = (idx h) + 1 }
+guessLetter g     h  = case safeRetr (solution h) (idx h) of 
+                        Nothing    -> h { idx = (idx h) + 1 }
+                        Just g     -> h { unhang = unhang h ++ [g], idx = (idx h) + 1 }
+                        
+                        
+                        
+safeRetr :: [a] -> Int -> Maybe a
+safeRetr []   _     = Nothing
+safeRetr l    n     = if n > length l then Nothing else Just $ l !! n
