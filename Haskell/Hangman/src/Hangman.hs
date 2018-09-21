41a2792edce731edfd2e744406b203df4b0b5697
michael.nyika <michael.nyika@king.com>
2018-09-21 10:24:58 +0200

    totally fine

--- Haskell/Hangman/src/Hangman.hs
+++ Haskell/Hangman/src/Hangman.hs
@@ -4,12 +4,14 @@ module Hangman where
     
 type UGuess = Char
 
-data HangWord    = HangWord { unhang   :: [Char],
+data HangWord    = HangWord { unhang   :: [UGuess],
                               idx      ::    Int,
-                              solution :: [Char],
+                              solution :: [UGuess],
                               chances  ::    Int
                             } deriving (Show)
 
+-- | What the player has guessed so far
+type PartialGuess = [Char]
 
 guessLetter :: (UGuess, [UGuess]) -> HangWord -> ([UGuess], HangWord)
 guessLetter (' ',gs )  h       = (gs,h)
