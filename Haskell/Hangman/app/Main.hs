d22566912af9f57a0679b5417ebe9f1ee33135f4
michael.nyika <michael.nyika@king.com>
2018-09-21 15:49:49 +0200

    ok so far

--- Haskell/Hangman/app/Main.hs
+++ Haskell/Hangman/app/Main.hs
@@ -1,6 +1,32 @@
 module Main where
 
 import Hangman
+import HangmanVisual
+
+solutionword = "constable"
 
 main :: IO ()
-main = undefined
+main = do putStrLn "Welcome To Haskell's Hangman"
+          let mask      = hideWords solutionword
+              initHang  = HangWord {uhang = mask, chances = length hangover} in 
+               runHangman initHang 0
+
+
+-- | Function into the wonderful world of hangman
+runHangman :: HangWord -> HangStart -> IO ()
+runHangman h s = 
+ do guess <- getChar 
+    let ch = chances h
+        h' = guessLetter (guess, mapify solutionword) h in 
+                 case chances h' == 0 of 
+                   True   -> do  putStrLn $ hangover !! s
+                                 putStrLn "´´´´´´´´´´´´´´" 
+                                 putStrLn "  GAME OVER   " 
+                                 putStrLn "``````````````" 
+                                 return ()
+                   False  -> do case chances h' == ch     of
+                                  True  -> runHangman h' s
+                                  False -> do putStrLn $ hangover !! s
+                                              runHangman h' (s+1)
+                    
+                 
--- Haskell/Hangman/src/Hangman.hs
+++ Haskell/Hangman/src/Hangman.hs
@@ -4,12 +4,16 @@ module Hangman where
 
 import Data.Foldable
 import Data.Hashable   (Hashable)
-import qualified Data.HashMap as H   (Map, toList, delete, insert)
+import Data.Char
+import qualified Data.HashMap as H   (Map, toList, fromList, delete, insert, empty)
 
 -- | Represents an index position of a single character in a string
 type Idx    = Int 
 -- | Represents a guess by a player, which is always a single character at a time
 type UGuess = Char
+
+-- | Starting position of the visual output for the player
+type HangStart = Idx
 
 -- | If any word (the solution, or word the user is supposed to guess and get right) is represented as a string of characters, then the keys represent the indices of the occurrences of some character that composes the word. As the player guesses, the type's internal structure will change
 type Solution    = H.Map [Idx] Char                    
@@ -17,7 +21,7 @@ type Solution    = H.Map [Idx] Char
 -- | Represents the progress of the player making guesses as the game progresses
 data HangWord    = HangWord { uhang    ::  [UGuess],  -- <-- The Guess Word Structure so far constructed by Hangman
                               chances  ::  Int        -- <-- The number of tries left before we get hanged (0 = lights out)
-                            } deriving (Show)
+                            } deriving (Show, Eq)
 
 -- | Formal function to process the guessed letter. UGuess represents the player's guess, 
 --   the Solution represents a map of the dictionary word with character indices. Not making a guess (by 
@@ -42,9 +46,37 @@ jury  g s    =
                       w@(i:[])  -> Just (i, H.delete w s)
                       w@(i:is)  -> Just (i, H.insert is g $ H.delete w s)
  
--- | function to find the indices that represent where that character appears in the solution string. 
+-- | Function to find the indices that represent where that character appears in the solution string. 
 --   If it cannot be found, it means the guess was wrong and we return Nothing, otherwise we return the 
 --   key in the solutionMap 
 locateGuess :: UGuess -> [([Idx], Char)] -> Maybe [Idx]
 locateGuess  _   []           = Nothing
 locateGuess  g   ((ix,v):xs)  = if g == v then (Just ix) else locateGuess g xs
+
+-- | Function that replaces all characters in a solution string with placeholders (so player can see how many characters the word is comprised of, and start guessing that the characters are at the placeholders)
+hideWords :: [UGuess] -> [UGuess]
+hideWords s = (\_ -> '_') <$> s
+
+-- | Function to take a word (the solution to the word puzzle), and creates a map; the keys represent the indices
+--   of the occurrences of each character value in the list 
+mapify :: [UGuess] -> Solution
+mapify []  = H.empty
+mapify  s  = H.fromList $ crunchMap 0 s
+
+-- | Function to create a list of tuples; first element of each tuple is a list of indexes, in the order in which 
+--   characters appear in a list; second element is the character itself; the first parameter is the starting index
+--   of the first character in a list
+crunchMap :: Idx -> [UGuess] -> [([Idx], Char)]
+crunchMap _  []       = []
+crunchMap n (s:xs)  = ([n],s) : crunchMap (n+1) xs
+
+
+-- | Function that combines entries of indice lists, if the character is the same for 2 elements
+simplify :: [([Idx], Char)] -> [([Idx], Char)]
+simplify ((l,c):es) = let dups = filter (\(_,c') -> c == c') es
+                          finl = (\(l',_) -> l') <$> dups in 
+                                 ((l++(concat finl)), c) : simplify es
+
+-- | Function to present the progress so far
+modProgress :: [UGuess] -> [UGuess]
+modProgress p = (\c -> toUpper c) <$> p
