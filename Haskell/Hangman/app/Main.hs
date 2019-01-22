c0fc52601b6244569f77fa3a9ec408d7d6b04b13
michael.nyika <michael.nyika@king.com>
2019-01-22 15:57:28 +0100

    ok

--- Haskell/Hangman/app/Main.hs
+++ Haskell/Hangman/app/Main.hs
@@ -18,24 +18,6 @@ import qualified Data.HashMap as H   (empty)
 
 solutionword = "mississippi"
 
-{-
-main :: IO ()
-main = do welcome
-          let mask      = hideWords solutionword
-              initHang  = HangWord {uhang = mask, chances = length hangover} in 
-               do putStrLn ("You start with "++(show $ length hangover)++" Chances! ")
-                  putStrLn ("Word Layout : ["++intersperse ' ' mask++"]")
-                  runHangman initHang 0 (mapify solutionword)
--}
-
--- | Comonadic version
-main :: IO ()
-main = do welcome
-          let mask      = hideWords solutionword
-              initHang  = HangStuff {g='#', c = Chances {ch = length hangover, uh = mask, sol = mapify solutionword }, idx = 0} in 
-               do putStrLn ("You start with "++(show $ length hangover)++" Chances! ")
-                  putStrLn ("Word Layout : ["++intersperse ' ' mask++"]")
-                  runHangmanC initHang
 
 
 -- | COMONADIC VERSION :: Function into the wonderful world of hangman
@@ -71,13 +53,7 @@ runHangman h                                  s sol  =
                                                                      (modProgress $ uhang h') (chances h')
                                                                   runHangman h' (s+1) sol'
 
--- | Herzlich Wilkommen                                                                  
-welcome :: IO ()
-welcome = do putStrLn "Welcome To Haskell's Hangman"
-             putStrLn ""
-             putStrLn logo
-             putStrLn ""
-          
+
 -- | Print to the console the progress on the gallows and the word building
 showProgress :: String -> String -> Int -> IO ()
 showProgress gallows w c = 
@@ -96,3 +72,29 @@ gatherInput = do putStrLn ""
                  putStrLn ""
                  return guess
                 
+{-
+main :: IO ()
+main = do welcome
+          let mask      = hideWords solutionword
+              initHang  = HangWord {uhang = mask, chances = length hangover} in 
+               do putStrLn ("You start with "++(show $ length hangover)++" Chances! ")
+                  putStrLn ("Word Layout : ["++intersperse ' ' mask++"]")
+                  runHangman initHang 0 (mapify solutionword)
+-}
+
+-- | Comonadic version
+main :: IO ()
+main = do welcome
+          let mask      = hideWords solutionword
+              initHang  = HangStuff {g='#', c = Chances {ch = length hangover, uh = mask, sol = mapify solutionword }, idx = 0} in 
+               do putStrLn ("You start with "++(show $ length hangover)++" Chances! ")
+                  putStrLn ("Word Layout : ["++intersperse ' ' mask++"]")
+                  runHangmanC initHang
+
+-- | Herzlich Wilkommen                                                                  
+welcome :: IO ()
+welcome = do putStrLn "Welcome To Haskell's Hangman"
+             putStrLn ""
+             putStrLn logo
+             putStrLn ""
+          
